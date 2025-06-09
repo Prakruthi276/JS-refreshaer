eaea99770279521bb3009d6dcf366f683b3c2b85
Prakruthi M V <4bd22cs112@bietdvg.edu>
2025-06-09 21:25:01 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import About from './components/About';
+import { useState } from 'react';
+
+let name="Harry";
+function App() {
+  const[mode,setMode]=useState('light');
+
+  const toggleMode=()=>{
+    if (mode==='light'){
+      setMode('dark');
+      document.body.style.backgroundColor = 'grey';
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+
+  }
+  
+  return (
+      <>
+    <Navbar  title="textUtils" mode={mode} toggleMode={toggleMode}/>
+     <TextForm heading="enter the text to the container"  mode={mode}/>
+      {/*<About/>*/}
+      
+
+    
+
+     </>
+  );
+}
+
+export default App;
